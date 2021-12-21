3cf4679cb55af680b8038ef82be3e76fe4049f2d
MsWoo <minsoo010@naver.com>
2021-12-22 00:37:46 +0900

    add rsi

--- src/modules.py
+++ src/modules.py
@@ -89,3 +89,31 @@ def send_line_message(message, line_token):
 
     except Exception:
         raise
+		
+def get_rsi(candle_data):
+    try:
+        df = pd.DataFrame(candle_data)
+        df = df.reindex(index=df.index[::-1]).reset_index()
+ 
+        df['close'] = df["trade_price"]
+ 
+        # RSI 계산
+        def rsi(ohlc: pd.DataFrame, period: int = 14):
+            ohlc["close"] = ohlc["close"]
+            delta = ohlc["close"].diff()
+ 
+            up, down = delta.copy(), delta.copy()
+            up[up < 0] = 0
+            down[down > 0] = 0
+ 
+            _gain = up.ewm(com=(period - 1), min_periods=period).mean()
+            _loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
+ 
+            RS = _gain / _loss
+            return pd.Series(100 - (100 / (1 + RS)), name="RSI")
+ 
+        rsi = round(rsi(df, 14).iloc[-1], 4)
+        return rsi
+ 
+    except Exception:
+        raise
